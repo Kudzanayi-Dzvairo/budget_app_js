8ffa72a1cce1a5307ccf15bc1aaa94307d3b3113
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-05-29 02:27:52 -0400

    form functionality complete

--- js/app.js
+++ js/app.js
@@ -33,10 +33,56 @@ class UI {
   }
 
   //submit expense form 
-  submitExpenseFrom(){
-    
-  }
+  submitExpenseForm(){
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+    if(expenseValue === '' || amountValue === '' || amountValue < 0){
+      this.expenseFeedback.classList.add('showItem')
+      this.expenseFeedback.innerHTML = `<p>values cannot be empty or negative</p>`
+      setTimeout(() => {
+        this.expenseFeedback.classList.remove('showItem')
+      },4000)
+    }
+    else{
+      let amount = parseInt(amountValue);
+      this.expenseInput .value = '';
+      this.amountInput.value = '';
 
+      let expense = {
+        id:this.itemID,
+        title:expenseValue,
+        amount:amount,
+      }
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense)
+      this.showBalance()
+    }
+  }
+  
+  //add expenxe
+  addExpense(expense){
+     const div = document.createElement('div');
+     div.classList.add('expense');
+     div.innerHTML = ` <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+     <h6 class="expense-title mb-0 text-uppercase list-item">-${expense.title}</h6>
+     <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+     <div class="expense-icons list-item">
+
+      <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+       <i class="fas fa-edit"></i>
+      </a>
+      <a href="#" class="delete-icon" data-id="${expense.id}">
+       <i class="fas fa-trash"></i>
+      </a>
+     </div>
+    </div>`
+    ;
+    this.expenseList.appendChild(div);
+  }
+  
   //show balance 
   showBalance(){
     const expense = this.totalExpense();
@@ -58,7 +104,14 @@ class UI {
 
   //total expense
   totalExpense(){
-    let total = 400;
+    let total = 0;
+    if(this.itemList.length>0){
+     total = this.itemList.reduce((acc, curr)=>{
+      acc += curr.amount;
+      return acc  
+      },0) 
+    }
+    this.expenseAmount.textContent = total;
     return total;
   }
 }
